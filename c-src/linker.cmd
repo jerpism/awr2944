04b2f208adc5e7c91c421cccaec4858d55ee87d0
jerpism <jere.m@live.fi>
2025-09-24 15:22:17 +0300

    Copy linker script from enet example to make it work.

--- c-src/linker.cmd
+++ c-src/linker.cmd
@@ -1,3 +1,4 @@
+#include "ti_enet_config.h"
 
 /* This is the stack that is used by code running within main()
  * In case of NORTOS,
@@ -11,7 +12,7 @@
 /* This is the heap size for malloc() API in NORTOS and FreeRTOS
  * This is also the heap used by pvPortMalloc in FreeRTOS
  */
---heap_size=32768
+--heap_size=34000
 -e_vectors  /* This is the entry of the application, _vector MUST be plabed starting address 0x0 */
 
 /* This is the size of stack when R5 is in IRQ mode
@@ -60,6 +61,22 @@ SECTIONS
         .data:   {} palign(8)   /* This is where initialized globals and static go */
     } > MSS_L2
 
+    GROUP {
+        .lwip_pools:  (NOLOAD)
+        {
+            . = align(128);
+            *(*memp_memory_POOL_1792_base*)
+            . = align(128);
+            *(*memp_memory_POOL_4096_base*)
+            . = align(128);
+            *(*memp_memory_POOL_128_base*)
+            . = align(128);
+            *(*memp_memory_PBUF_POOL_base*)
+        } palign(128)
+    } > DSS_L3
+
+    
+
     /* This is rest of uninitialized data. This can be placed in DDR if DDR is available and needed */
     GROUP {
         .bss:    {} palign(8)   /* This is where uninitialized globals go */
@@ -99,66 +116,33 @@ SECTIONS
     /* any data buffer needed to be put in L3 can be assigned this section name */
     .bss.dss_l3 {} > DSS_L3
 
-    /* General purpose user shared memory, used in some examples */
-  //  .bss.user_shared_mem (NOLOAD) : {} > USER_SHM_MEM
-    /* this is used when Debug log's to shared memory are enabled, else this is not used */
-//    .bss.log_shared_mem  (NOLOAD) : {} > LOG_SHM_MEM
-    /* this is used only when IPC RPMessage is enabled, else this is not used */
-    .bss.ipc_vring_mem   (NOLOAD) : {} > RTOS_NORTOS_IPC_SHM_MEM
-    /* this is used only when Secure IPC is enabled */
-    .bss.sipc_hsm_queue_mem   (NOLOAD) : {} > MAILBOX_HSM
-    .bss.sipc_r5f_queue_mem   (NOLOAD) : {} > MAILBOX_R5F
-
-    // For NDK packet memory
+    /* For NDK packet memory*/
     .bss:ENET_CPPI_DESC        (NOLOAD) {} ALIGN (128) > CPPI_DESC
 
-        .enet_dma_mem{
+    .enet_dma_mem {
+#if (ENET_SYSCFG_PKT_POOL_ENABLE == 1)
         *(*ENET_DMA_PKT_MEMPOOL)
+#endif
     } (NOLOAD) {} ALIGN (128) > DSS_L3
     .bss:UDP_IPERF_SND_BUF  (NOLOAD) {} ALIGN (128) > DSS_L3
-
 }
-/* L2 memory map
- *     112K       16K        736K        16K           16K
- *   0x1C000     0x4000    0xB8000      0x4000       0x4000
- *  +----------------------------------------------------------+
- *  | SBL RES | CPPI DESC | MSS_L2 | USR_SHM_MEM | LOG_SHM_MEM |
- *  +----------------------------------------------------------+
- *  |         |           |        |             |             |
- * 0x10200000 0x1021C000  |        0x102D8000    0x102DC000    0x102E0000
- *                        0x10220000         
- */                        
 
 MEMORY
 {
     R5F_VECS  : ORIGIN = 0x00000000 , LENGTH = 0x00000040
-    R5F_TCMA  : ORIGIN = 0x00000040 , LENGTH = 0x0000FFC0
-    R5F_TCMB  : ORIGIN = 0x00080000 , LENGTH = 0x00010000
-    SBL_RESERVED_L2_RAM (RW)   : origin=0x10200000 length= 0x00040000
+    R5F_TCMA  : ORIGIN = 0x00000040 , LENGTH = 0x00003FC0
+    R5F_TCMB  : ORIGIN = 0x00080000 , LENGTH = 0x00004000
+    SBL_RESERVED_L2_RAM (RW)   : origin=0x10200000 length=(0x00020000 - 0x00004000)
+
 
     /* CPPI descriptor memory */
-    CPPI_DESC : ORIGIN = 0x10240000, LENGTH = 0x00004000
+    CPPI_DESC : ORIGIN = (0x10220000 - 0x00004000), LENGTH = 0x00004000
 
     /* when using multi-core application's i.e more than one R5F active, make sure
      * this memory does not overlap with other R5F's
      */
-    MSS_L2      : ORIGIN = (0x10244000) , LENGTH = 0x400000
+    MSS_L2     : ORIGIN = 0x10220000 , LENGTH = 0x000D0000
 
-
-    /* shared memories that are used by RTOS/NORTOS cores */
-    /* On R5F,
-     * - make sure there are MPU entries which maps below regions as non-cache
-     * Might not be needed so these could be removed to get 32K extra for MMS_L2
-     */
-    //USER_SHM_MEM            : ORIGIN = (0x102DC000 - 0x4000), LENGTH = 0x00004000
-    //LOG_SHM_MEM             : ORIGIN = (0x102E0000 - 0x4000), LENGTH = 0x00004000
-
-    /* This is typically used to hold data IO buffers from accelerators like CSI, HWA */
-    DSS_L3:   ORIGIN = 0x88000000, LENGTH = 0x000F0000
-
-    /* 1st 512 B of DSS mailbox memory and MSS mailbox memory is used for IPC with R4 and should not be used by application */
-    /* MSS mailbox memory is used as shared memory, we dont use bottom 32*6 bytes, since its used as SW queue by ipc_notify */
-    RTOS_NORTOS_IPC_SHM_MEM : ORIGIN = 0xC5000200, LENGTH = 0xD40
-    MAILBOX_HSM:    ORIGIN = 0x44000000 , LENGTH = 0x000003CE
-    MAILBOX_R5F:    ORIGIN = 0x44000400 , LENGTH = 0x000003CE
+    /* This is typically used to hold data IO buffers from accelerators like CSI, HWA, DSP */
+    DSS_L3:   ORIGIN = 0x88000000, LENGTH = 0x00280000
 }
