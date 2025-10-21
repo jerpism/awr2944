1e6a13d535eafdec10c82e76992926fb82e3b066
jerpism <jere.m@live.fi>
2025-10-21 18:45:15 +0300

    add radarcube_t and detmatrix_t to types.h

--- c-src/include/types.h
+++ c-src/include/types.h
@@ -1,6 +1,7 @@
 #ifndef TYPES_H
 #define TYPES_H
 #include <stdint.h>
+#include <cfg.h>
 
 // Might (likely) be used as the type for complex samples.
 // Right now there is a dangerous mix of using (u)int16_t and uint32_t to handle complex samples
@@ -15,5 +16,8 @@ typedef struct __attribute__((packed)) {
     int16_t re;
     int16_t im;
  }int16reim_t;
+
+ typedef int16imre_t radarcube_t[NUM_TX_ANTENNAS][NUM_DOPPLER_CHIRPS][NUM_RX_ANTENNAS][NUM_RANGEBINS];
+ typedef uint16_t detmatrix_t[NUM_RANGEBINS][NUM_DOPPLER_CHIRPS];
 
 #endif /* TYPES_H */
