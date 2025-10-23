52702c038f95fefbf0bc84309a15628f08851c92
jerpism <jere.m@live.fi>
2025-10-23 18:09:45 +0300

    Add cfar functions to dataprocessing header.

--- c-src/include/dataprocessing.h
+++ c-src/include/dataprocessing.h
@@ -4,6 +4,9 @@
 #include <types.h>
 
 void dp_run_doppler(radarcube_t data, detmatrix_t out);
-int dp_run_cfar(detmatrix_t detmatrix);
 void dp_run_anglefft(radarcube_t data, struct detected_point *points, size_t len, int16imre_t *out);
+
+int dp_run_cfar(detmatrix_t detmatrix);
+int dp_run_anglecfar(int16imre_t *angles, size_t len);
+
 #endif /* DATAPROCESSING_H */
