4e253a2fe9bc7293d697fe7d4b370f5b1bcce320
jerpism <jere.m@live.fi>
2025-11-21 09:14:42 +0200

    Add functions to output CFAR data via serial.

--- c-src/uart.c
+++ c-src/uart.c
@@ -3,9 +3,52 @@
 #include <string.h>
 #include <drivers/uart.h>
 #include <stdint.h>
+#include <types.h>
 
 #include "ti_drivers_config.h"
 #include "ti_drivers_open_close.h"
+
+// Patented screen clear (patent pending)
+static char graphic[512];
+static inline void construct_monstrosity(){
+    memset(graphic, '\n', 100);
+    graphic[100] = '\r';
+}
+
+void uart_graphic(struct detected_point *list, size_t n){
+    UART_Transaction trans;
+    UART_Transaction_init(&trans);
+
+    construct_monstrosity();
+
+    trans.buf = graphic;
+    trans.count = 229;
+    memset(graphic + 101, '.', 128);
+    int offset = 101;
+    for(int i = 0; i < n; ++i){
+        graphic[offset + list[i].range] = '*'; 
+    }
+
+    UART_write(gUartHandle[0], &trans);
+
+}
+
+void uart_dump_detections(struct detected_point *list, size_t n){
+    // Needs to hold a "%hu,%hu\r\n" string
+    char buff[32];
+    UART_Transaction transaction;
+
+    int maxlen = snprintf(NULL, 0, "%hu,%hu\r\n", (uint16_t)UINT16_MAX, (uint16_t)UINT16_MAX);
+
+    UART_Transaction_init(&transaction);
+    transaction.buf = buff;
+    for(int i = 0; i < n; ++i){
+        transaction.count = snprintf(buff, maxlen, "%hu,%hu\r\n",list[i].range, list[i].doppler);
+        UART_write(gUartHandle[0], &transaction);
+    }
+
+
+}
 
 
 // Writes out samples in a CSV format to the serial console
